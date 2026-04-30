c14559ebae276ab24151407a35d85c938e692694
Егор <92874412+EgorchikF@users.noreply.github.com>
2026-04-30 22:08:12 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace VM_Wrae_launcher
+{
+    partial class LaucnherForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LaucnherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(160, 115);
+            this.ControlBox = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LaucnherForm";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Load += new System.EventHandler(this.LaucnherForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VM_Wrae_launcher
+{
+    public partial class LaucnherForm : Form
+    {
+        public LaucnherForm()
+        {
+            InitializeComponent();
+        }
+
+        async void LaucnherForm_Load(object sender, EventArgs e)
+        {
+            // Список служб для управления ими
+            string[] services = { "VMAuthdService", "VMwareAutostartService", "VMnetDHCP", "VMware NAT Service", "VMUSBArbService" };
+
+            // Перевод служб в режим "вручную"
+            foreach (var service in services)
+            {
+                cmd($"sc config \"{service}\" start= demand");
+            }
+            // Запуск служб VMware
+            foreach (var service in services)
+            {
+                cmd($"sc start \"{service}\"");
+            }
+
+            // Запуск необходимых процессов
+            cmd("start \"\" \"C:\\Windows\\SysWOW64\\vmnetdhcp.exe\"");
+            cmd("start \"\" \"C:\\Windows\\SysWOW64\\vmnat.exe\"");
+            cmd("start \"\" \"%PROGRAMFILES(X86)%\\VMware\\VMware Workstation\\vmware.exe\"");
+
+            // Ожидание закрытия VMware Workstation
+            while (Process.GetProcessesByName("vmware").Length > 0)
+            {
+                await Task.Delay(2000); // Проверка каждые 2 секунды
+            }
+
+            // Завершение всех процессов VMware
+            cmd("taskkill /f /im vmware-autostart.exe & " + "taskkill /f /im vmnetdhcp.exe & " +"taskkill /f /im vmware-usbarbitrator64.exe & " 
+                + "taskkill /f /im vmware-authd.exe & " +"taskkill /f /im vmnat.exe");
+
+            // Остановка служб
+            foreach (var service in services)
+            {
+                cmd($"sc stop \"{service}\"");
+            }
+
+            // Перевод служб в режим "отключено"
+            foreach (var service in services)
+            {
+                cmd($"sc config \"{service}\" start= disabled");
+            }
+
+            // Завершение текущего процесса (лаунчера)
+            Application.Exit();
+        }
+
+        void cmd(string line)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c {line}",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true, // Скрыть окно
+                UseShellExecute = false // Не использовать оболочку
+            };
+
+            using (Process process = Process.Start(processInfo))
+            {
+                process.WaitForExit();  // Ожидание завершения команды
+            }
+        }
+    }
+}
